01c6e084fc5cfd967b2f60c7861547882c033af8
Robert Norris <rob@eatenbyagrue.org>
2014-05-21 20:12:56 +1000

    handle messages to offline resource slightly more standards-compliantly

--- lib/DJabberd/Delivery/Local.pm
+++ lib/DJabberd/Delivery/Local.pm
@@ -2,6 +2,7 @@ package DJabberd::Delivery::Local;
 use strict;
 use warnings;
 use base 'DJabberd::Delivery';
+use Scalar::Util qw(blessed);
 
 sub run_before { ("DJabberd::Delivery::S2S") }
 
@@ -21,6 +22,17 @@ sub deliver {
         if (($dest = $vhost->find_jid($to)) && ($dest->is_available || $stanza->deliver_when_unavailable)) {
             push @dconns, $dest;
         } else {
+
+            # specific resource request, but not available. what we do depends on stanza type
+            # XXX stanza classes themselves should hold this logic
+            if (blessed $stanza eq "DJabberd::Message") {
+                my $type = $stanza->{attrs}{"{}type"};
+                if ($type =~ m/^(?:normal|groupchat|headline)$/) {
+                    $stanza->make_error_response('503', 'cancel', 'service-unavailable')->deliver($vhost);
+                    return $cb->delivered;
+                }
+            }
+
             $find_bares->();
         }
     }
